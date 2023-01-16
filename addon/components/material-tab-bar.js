53c75f0087a276fe4a5c5d120df48ac16d0cd38c
Michael Wenting <michael.wenting@fabriquartz.com>
2023-01-16 15:30:28 +0100

    ok

--- addon/components/material-tab-bar.js
+++ addon/components/material-tab-bar.js
@@ -1,10 +1,17 @@
 import Component from '@glimmer/component';
 import { MDCTabBar } from '@material/tab-bar';
 import { action } from '@ember/object';
-
+import { tracked } from '@glimmer/tracking';
 export default class MaterialTabBarComponent extends Component {
   @action
   didInsert(element) {
-    new MDCTabBar(element);
+    this.tabBar = new MDCTabBar(element);
+  }
+
+  @action
+  activateTab(tabindex) {
+    if (this.tabBar) {
+      this.tabBar.activateTab(tabindex);
+    }
   }
 }
