29f9ea894871f5b887c9db68b1691cab6f6452a7
Michael Wenting <michaelwenting@live.nl>
2023-01-26 11:46:04 +0100

    Material Drawer + Top Bar

---
+++ tests/dummy/app/components/demo-material-drawer.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class DemoMaterialDrawerComponent extends Component {
+  @service settings;
+
+  @action
+  onClick(value) {
+    console.log('value--', value);
+    this.settings.materialDrawerModal = value;
+  }
+}
