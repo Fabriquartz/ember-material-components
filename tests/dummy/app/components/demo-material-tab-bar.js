ae7be24c00b518fb5cd4cdf29c8b037d8ec70bc3
Michael <michaelwenting@live.nl>
2023-02-03 16:04:20 +0100

    Added material-tab-bar, including demo page (#14)

---
+++ tests/dummy/app/components/demo-material-tab-bar.js
@@ -0,0 +1,23 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { next } from '@ember/runloop';
+
+export default class DemoMaterialTabBarComponent extends Component {
+  @tracked _tabBar = true;
+  @tracked icon = true;
+  @tracked dense = false;
+  @tracked stacked = false;
+  @tracked scrollable = false;
+
+  @action
+  update(path, value) {
+    this._tabBar = false;
+
+    this[path] = value;
+
+    next(() => {
+      this._tabBar = true;
+    });
+  }
+}
