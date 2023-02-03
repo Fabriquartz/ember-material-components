40a094d1a9fd9ee93607d331bc2f54ef7e88dcc0
Michael Wenting <michaelwenting@live.nl>
2023-02-03 16:17:15 +0100

    Add material menu

---
+++ addon/components/material-menu.js
@@ -0,0 +1,18 @@
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
+import { MDCMenu } from '@material/menu';
+import { tracked } from '@glimmer/tracking';
+
+export default class MaterialMenuComponent extends Component {
+  @tracked menu;
+  
+  @action
+  didInsert(element) {
+    this.menu = new MDCMenu(element);
+  }
+
+  @action
+  open() {
+    this.menu.open = true;
+  }
+}
