e8d9dfc8d344f08f2c4e7c41f8377764ed6709f9
Pieter <Pieter901@hotmail.com>
2023-02-03 11:46:41 +0100

    Update demo-material-switch.js

--- tests/dummy/app/components/demo-material-switch.js
+++ tests/dummy/app/components/demo-material-switch.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+
 export default class DemoMaterialSwitchComponent extends Component {
   @tracked switch1 = true;
   @tracked switch2 = true;
