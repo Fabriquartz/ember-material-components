5aa23b4df06783b28e7fbba7e1520aa0fcfa46f3
Michael Wenting <michaelwenting@live.nl>
2023-03-08 16:10:42 +0100

    Remove console log

--- addon/components/material-text-field/textarea.js
+++ addon/components/material-text-field/textarea.js
@@ -4,7 +4,6 @@ import Component from '@glimmer/component';
 export default class MaterialTextareaInputComponent extends Component {
   @action
   updateLayout() {
-    console.log('----', this.args.textField);
     if (document.activeElement !== this.args.textField.input) {
       this.args.textField.foundation.deactivateFocus();
     }
