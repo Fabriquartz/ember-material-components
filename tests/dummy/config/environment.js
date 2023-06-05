7d2a2db1e659c93cf439c910763844bf65134105
Pieter <Pieter901@hotmail.com>
2023-06-05 13:32:11 +0200

    Fix

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -42,7 +42,7 @@ module.exports = function (environment) {
 
   if (environment === 'production') {
     // Allow ember-cli-addon-docs to update the rootURL in compiled assets
-    ENV.rootURL = '/';
+    ENV.rootURL = '/ember-material-components/';
     // here you can enable a production-specific feature
   }
 
