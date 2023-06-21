67b24bfdbd18e586260aa50c62148d785831b1a3
Michael Wenting <michaelwenting@live.nl>
2023-06-21 14:04:06 +0200

    Bugfix for Switch in Material List

--- tests/dummy/app/controllers/docs/list.js
+++ tests/dummy/app/controllers/docs/list.js
@@ -6,6 +6,7 @@ import { tracked } from '@glimmer/tracking';
 export default class DocsListController extends Controller {
   @tracked value1 = false;
   @tracked value2 = false;
+  @tracked value3 = true;
 
   get doneCount() {
     return `${[this.value1, this.value2].filter((value) => value).length}/2`;
@@ -14,6 +15,11 @@ export default class DocsListController extends Controller {
   @action
   update(path, e) {
     this[path] = e.target.checked;
+  }
+
+  @action
+  onChange(path, value) {
+    this[path] = value;
   }
 }
 // END-SNIPPET
